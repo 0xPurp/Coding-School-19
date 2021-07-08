ccf5d2f4c7f480aa029c47b3c30c517e409ffbb0
0xPurp <haroune1020@hotmail.com>
2021-07-08 11:43:13 +0200

    Ajout exo 1 & 2 Laravel

---
+++ laravel/exo2/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Exo 2 - Laravel</title>
+</head>
+<body>
+    <h1> {{$var1}} {{$var2}} </h1>
+</body>
+</html>
---
+++ laravel/exo2/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $var1 = "Variable1";
+    $var2 = "Variable2";
+    return view('welcome', compact('var1', 'var2'));
+});
